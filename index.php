ba39702ab85e3a2af25291b86b2a2cc30052f22f
itsMeVela <velaputri681@gmail.com>
2023-08-23 21:01:52 +0700

    email

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Document</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Montserrat:wght@300&family=Noto+Sans:wght@300&family=Playfair+Display:wght@400;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -157,6 +154,23 @@
             </div>
         </div>
     </div>
+
+    
+        <div class="container" style="margin-top:18%;">
+            <div class="row" style="padding-top: 10%; padding-bottom: 10%;">
+                <div class="row">
+                    <div class="col-5">
+                        <h2 class="text-warning" style="font-size: 60px;"> Connect With Me</h2>
+                        <div class="input-group mb-3">
+                            <input type="text " class="form-control" placeholder="Email" aria-label="Email">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Messge" aria-label="Messge">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
