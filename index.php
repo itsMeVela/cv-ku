0dd68dd6349f60bf05c2448080756f483a6f3b83
itsMeVela <velaputri681@gmail.com>
2023-08-23 22:22:23 +0700

    email

--- index.php
+++ index.php
@@ -156,19 +156,19 @@
     </div>
 
 
-    <div class="container" style="margin-top:18%;">
+    <div class="container" style="margin-top:15%;">
         <div class="row" style="padding-top: 10%; padding-bottom: 10%;">
             <div class="row">
-                <div class="col-5">
-                    <h2 class="text-warning" style="font-size: 60px;"> Connect With Me</h2>
-                    <div class="input-group mb-3">
-                        <input type="text " class="form-control" placeholder="Email" aria-label="Email">
+                <div class="col-8">
+                    <h2 class="text-warning pb-3 " style="font-size: 80px;"> Connect With Me</h2>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control shadow-sm p-3 mb-5 bg-body-tertiary rounded" placeholder="Email" aria-label="Email">
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Messge" aria-label="Messge">
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control shadow-sm p-3 mb-5 bg-body-tertiary rounded" placeholder="Messge" aria-label="Messge">
                     </div>
                     <div>
-                        <button type="button" class="btn btn-outline-warning mt-4 btn-lg text-dark">Stay Connected <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <button type="button" class="btn btn-outline-warning btn-lg text-dark" style="margin-left: 75%;">Stay Connected <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                 <style>
                                     svg {
                                         fill: #1f2951
@@ -177,10 +177,16 @@
                                 <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
                             </svg> </button>
                     </div>
+                    <div class="container" style="margin-top:18%;">
+                        <div class="row" style="padding-top: 10%; padding-bottom: 10%;">
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
+                <div class="col-4">
+                    <div class="d-flex justify-content-center " style="margin-top: 15%; margin-left: 80%;">
+                        <image src="assets/img/3.png" alt="">
+                    </div>
+                </div>
 
 
 
