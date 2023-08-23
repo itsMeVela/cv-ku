558b4208ba1700e2676610f17a16a890e8ee77b5
ImFaldi <rifaldi.indrajaya@binus.ac.id>
2023-08-23 18:55:21 +0700

    Content Fixed

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <!-- https://www.behance.net/gallery/97205839/Developer-Portfolio-Design -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,74 +11,69 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Montserrat:wght@300&family=Noto+Sans:wght@300&family=Playfair+Display:wght@400;600&display=swap" rel="stylesheet">
 </head>
-<style>
-    .font
-    {
-        font-family: 'Inter', sans-serif;
-        font-family: 'Montserrat', sans-serif;
-        font-family: 'Noto Sans', sans-serif;
-        font-family: 'Playfair Display', serif;
-    }
-    .padding
-    {
-        margin-top: 5%;
-        margin-left: 7%;
-    }
-    body{background-color:rgb(26, 26, 34);}
-    .left
-    {
-        margin-left:7%;
-    }
 
-</style>
-    
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<body style="background-color: #EEEEEE;">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <image src="assets/img/Favel.png" class="d-inline-block align-text-top" alt="" style="width: 100px; height: 35px;">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link mx-3" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-3" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-3" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-3" href="#">Skill</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-3" href="#">Portofolio</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
-        <a class="navbar-brand" href="#">   <image src="assets/img/Favel.png"class="d-inline-block align-text-top" alt="" style="width: 100px; height: 35px;"></a>
-     
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link mx-3" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-3" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-3" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-3" href="#">Skill</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-3" href="#">Portofolio</a>
-                </li>
-            </ul>
+        <div class="row">
+            <div class="col-8" style="margin-top: 15%;">
+                <h1 style="font-size: 80px;">Hello, We're <br> a Front end Developer</h1>
+                <div class="d-inline-flex text-secondary mt-2">
+                    Fond of creating web application designs and<br>
+                    bring them to life using code and develop <br>
+                    mobile designs.
+                </div>
+                <div>
+                    <button type="button" class="btn btn-outline-warning mt-4 btn-lg text-dark">Creator Journey</button>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="d-flex justify-content-center" style="margin-top: 50%; margin-left: 70%;">
+                    <image src="assets/img/1.png" alt="">
+                </div>
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="left" style="margin-top:30%;">Services
+                <h1 class="text-warning" style="font-size: 60px;">Skill-Set</h1>
+                <div class="col-1 rounded" style="border: 3px solid #FFBD41;"></div>
+            </div>
         </div>
     </div>
-</nav>
 
-
-<body>
-   <div  >
-        <h1 class="font padding text-light ">Hello, We're <br> a Front end Developer</h1> <!--fontnya kok g ganti ya ayy:(-->
-        <div class="d-inline-flex p-2 font text-light" style="margin-left:7%;" >
-            Fond of creating web application designs and<br>
-            bring them to life using code and develop <br>
-            mobile designs. 
-         </div>
-         <div>
-         <button type="button" class="btn btn-warning btn-lg mt-2" style="margin-left:7%;" >Creator Journey</button>
-         </div>
-    </div class="font">
-   <div class=" text-light left" style="margin-top:10%;">Services
-    <h1 class="text-warning" >Skill-Set</h1>
-    
-   </div>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
